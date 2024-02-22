7c66468398e7f79b42a2d659657742ed0068a6f8
SoupaNut <nguytam745@gmail.com>
2024-02-21 21:17:44 -0600

    Update ProjectileBase.cs

--- VRLevelShooterGame/Assets/ProjectileBase.cs
+++ VRLevelShooterGame/Assets/ProjectileBase.cs
@@ -1,12 +1,24 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Unity.Game.Base
 {
-    public UnityAction OnShoot;
-
-    public class ProjectileBase : MonoBehaviour
+    public abstract class ProjectileBase : MonoBehaviour
     {
+        public Vector3 InitialPosition { get; private set; }
+        public Vector3 InitialDirection { get; private set; }
+        public Vector3 InheritedMuzzleVelocity { get; private set; }
 
+        public UnityAction OnShoot;
+
+        public void Shoot(WeaponController controller)
+        {
+            InitialPosition = transform.position;
+            InitialDirection = transform.forward;
+            InheritedMuzzleVelocity = controller.MuzzleWorldVelocity;
+
+            OnShoot?.Invoke();
+        }
     }
 }
 
